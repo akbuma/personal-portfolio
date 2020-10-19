e04e94de552bc2c822a67cf67e95f8fd60573549
Aaron Bumanglag <aaron.k.bumanglag@gmail.com>
2020-10-19 04:07:28 -0700

    add event listener function for window resize

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const menu = document.querySelector('.navbar__toggle');
 const menuLinks = document.querySelector('.navbar__menu');
-const navigation = document.querySelector('.navbar');
+const navbar = document.querySelector('.navbar');
 const header = document.getElementById('header');
 
-let mobileNavigationIsOn = false;
-
-const mobileMenu = () => {
-  mobileNavigationIsOn = !mobileNavigationIsOn;
+// animates navbar toggle and displays links for mobile
+const openMobileMenu = () => {
   menu.classList.toggle('navbar__toggle--is-active');
   menuLinks.classList.toggle('active');
-  if (mobileNavigationIsOn) {
-    header.appendChild(menuLinks);
-  } else {
-    navigation.appendChild(menuLinks);
-  }
 };
 
-menu.addEventListener('click', mobileMenu);
+// adds navbar__menu as child of header node for animation effects
+const repositionMenuLinks = () => {
+  window.outerWidth <= 960
+    ? header.appendChild(menuLinks)
+    : navbar.appendChild(menuLinks);
+};
+
+window.addEventListener('resize', repositionMenuLinks);
+menu.addEventListener('click', openMobileMenu);
