307349f9e86a7396fca9930b0569350e6973536b
Aaron Bumanglag <aaron.k.bumanglag@gmail.com>
2020-10-21 03:03:54 -0700

    replace sidebar transition with in-out animation

--- app.js
+++ app.js
@@ -2,20 +2,26 @@ const menu = document.querySelector('.navbar__toggle');
 const menuLinks = document.querySelector('.navbar__menu');
 const navbar = document.querySelector('.navbar');
 // const header = document.getElementById('header');
-const sideMenu = document.querySelector('.sidebar');
+// const sideMenu = document.querySelector('.sidebar');
 
 // animates navbar toggle and displays links for mobile
 const openMobileMenu = () => {
   menu.classList.toggle('navbar__toggle--is-active');
   menuLinks.classList.toggle('active');
-  sideMenu.classList.toggle('active');
+  // sideMenu.classList.toggle('active');
 };
 
 // adds navbar__menu as child of header node for animation effects
 const repositionMenuLinks = () => {
-  window.outerWidth <= 960
-    ? sideMenu.appendChild(menuLinks)
-    : navbar.appendChild(menuLinks);
+  // window.outerWidth <= 960
+  //   ? sideMenu.appendChild(menuLinks)
+  //   : navbar.appendChild(menuLinks);
+  if (window.outerWidth >= 960) {
+    if (menu.classList.contains('navbar__toggle--is-active'))
+      menu.classList.toggle('navbar__toggle--is-active');
+    if (menuLinks.classList.contains('active'))
+      menuLinks.classList.toggle('active');
+  }
 };
 
 window.addEventListener('resize', repositionMenuLinks);
