51bd9f6b00f6f8a7d72c93b0380dc52d63b36a58
Aaron Bumanglag <aaron.k.bumanglag@gmail.com>
2020-10-21 01:15:54 -0700

    refined sidebar transition

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const menu = document.querySelector('.navbar__toggle');
 const menuLinks = document.querySelector('.navbar__menu');
 const navbar = document.querySelector('.navbar');
-const header = document.getElementById('header');
+// const header = document.getElementById('header');
+const sideMenu = document.querySelector('.sidebar');
 
 // animates navbar toggle and displays links for mobile
 const openMobileMenu = () => {
   menu.classList.toggle('navbar__toggle--is-active');
   menuLinks.classList.toggle('active');
+  sideMenu.classList.toggle('active');
 };
 
 // adds navbar__menu as child of header node for animation effects
 const repositionMenuLinks = () => {
   window.outerWidth <= 960
-    ? header.appendChild(menuLinks)
+    ? sideMenu.appendChild(menuLinks)
     : navbar.appendChild(menuLinks);
 };
 
