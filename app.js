734a8c5229b1d0f4695b9819f07bcf43fccaa3c3
Aaron Bumanglag <aaron.k.bumanglag@gmail.com>
2020-10-29 01:22:04 -0700

    fixed stuttering issue with mobile nav

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
 const menu = document.querySelector('.navbar__toggle');
 const menuLinks = document.querySelector('.navbar__menu');
 const navbar = document.querySelector('.navbar');
-// const header = document.getElementById('header');
-// const sideMenu = document.querySelector('.sidebar');
 
 // animates navbar toggle and displays links for mobile
 const openMobileMenu = () => {
   menu.classList.toggle('navbar__toggle--is-active');
-  menuLinks.classList.toggle('active');
-  // sideMenu.classList.toggle('active');
+
+  // display the mobile nav
+  // toggling closed class will remove the stuttering issue when switching to mobile
+  if (menuLinks.classList.contains('active')) {
+    menuLinks.classList.toggle('closed');
+  } else {
+    menuLinks.classList.toggle('active');
+  }
 };
 
 // adds navbar__menu as child of header node for animation effects
 const repositionMenuLinks = () => {
-  // window.outerWidth <= 960
-  //   ? sideMenu.appendChild(menuLinks)
-  //   : navbar.appendChild(menuLinks);
   if (window.outerWidth >= 960) {
+    // disable toggle when user widens screen
     if (menu.classList.contains('navbar__toggle--is-active'))
       menu.classList.toggle('navbar__toggle--is-active');
+    // disable mobile menu when user widens screen
     if (menuLinks.classList.contains('active'))
       menuLinks.classList.toggle('active');
+    if (menuLinks.classList.contains('closed'))
+      menuLinks.classList.toggle('closed');
   }
 };
 
